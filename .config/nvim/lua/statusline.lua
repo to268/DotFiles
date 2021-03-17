f19f2a40a33ceaed5d423bb1a5851ec493c7eb1f
to268 <t.guillot268@gmail.com>
2021-03-17 23:33:22 +0100

    Updated lualine nvim config

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,30 +1,30 @@
 local lualine = require('lualine')
 
 local function statusline()
-    lualine.options = {
-        theme = 'onedark',
-        separator = '|',
-        icons_enabled = true,
+    require('lualine').setup{
+        options = {
+            theme = 'onedark',
+            section_separators = {'', ''},
+            component_separators = {'|', '|'},
+            icons_enabled = true,
+        },
+        sections = {
+            lualine_a = { {'mode', upper = true} },
+            lualine_b = { {'branch', icon = ''} },
+            lualine_c = { {'filename', file_status = true} },
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location'  },
+        },
+        inactive_sections = {
+            lualine_a = {  },
+            lualine_b = {  },
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = {  },
+            lualine_z = {   }
+        },
     }
-    lualine.separator = '|'
-    lualine.sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location'  },
-    }
-    lualine.inactive_sections = {
-        lualine_a = {  },
-        lualine_b = {  },
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {  },
-        lualine_z = {  },
-    }
-    lualine.extensions = { 'fzf' }
-    lualine.status()
 end
 
 return {

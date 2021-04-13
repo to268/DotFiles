fce48bdc6e0e5980dc3389fc52fd8e8bbccf207b
to268 <t.guillot268@gmail.com>
2021-04-13 12:11:02 +0200

    Modified Gimp Propeties in dwm config

--- .local/share/dwm/config.h
+++ .local/share/dwm/config.h
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           1 << 8,    1,          0,           0,        -1 },
+	{ "Gimp",    NULL,     NULL,           1 << 8,    0,          0,           1,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */

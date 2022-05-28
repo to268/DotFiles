a299fff4347bd8a52c91743f7b937eb5c0010f9c
Guillot Tony <tony.guillot@protonmail.com>
2022-05-28 09:24:22 +0200

    Fixed nvim-tree.lua nvim plugin

--- .config/nvim/lua/nvim-plugin/nvim_tree.lua
+++ .config/nvim/lua/nvim-plugin/nvim_tree.lua
@@ -8,49 +8,6 @@ require('nvim-tree').setup {
     open_on_tab         = false,
     hijack_cursor       = true,
     update_cwd          = false,
-    diagnostics = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-        }
-    },
-
-    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-    update_focused_file = {
-        -- enables the feature
-        enable      = true,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
-        update_cwd  = false,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-        ignore_list = {}
-    },
-
-    -- configuration options for the system open command (`s` in the tree by default)
-    system_open = {
-        -- the command to run this, leaving nil should work in most cases
-        cmd  = nil,
-        -- the command arguments as a list
-        args = {}
-    },
-
-    view = {
-        -- width of the window, can be either a number (columns) or a string in `%`
-        width = '15%',
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        side = 'left',
-        mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = false,
-            -- list of mappings to set on the tree manually
-            list = {}
-        }
-    }
 }
 
 map("n", "<C-n>", ":lua require('nvim-tree').toggle()<CR>", { noremap = true, silent = true })
